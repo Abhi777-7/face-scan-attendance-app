c0d282f17c04dd12e15a43a648d0014ed2e67cc2
POKANATI ABHINAY <abhinaypokanati@gmail.com>
2025-07-20 16:49:57 +0530

    Add .next/static/chunks/src_app_scan_page_tsx_9148afbf._.js 1753010396016

---
+++ .next/static/chunks/src_app_scan_page_tsx_9148afbf._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_@tensorflow_tfjs-core_dist_tf-core_esm_e127b7ab.js",
+  "static/chunks/node_modules_face-api_js_build_es6_5e73a2c6._.js",
+  "static/chunks/node_modules_4e1c6573._.js",
+  "static/chunks/src_f6a7d5dc._.js"
+],
+    source: "dynamic"
+});
